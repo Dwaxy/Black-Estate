867bf5e9447808a2240c37e05c413ce01ac0e61f
Joel <joel.innes@gmail.com>
2019-09-04 10:32:30 +0100

    updates (one one I forgot to push)

--- js/lib/nav.js
+++ js/lib/nav.js
@@ -1,13 +1,44 @@
 
 
+
+var navOn = true
+$("#mobile-toggle").click( function() {
+  if(navOn) {
+    $(".nav-holder").fadeOut("fast"
+    navOn = false
+  } else if(!navOn) {
+    $(".nav-holder").fadeIn("fast")
+    navOn = true
+  }
+})
+
 // code from https://stackoverflow.com/questions/8189840/get-mouse-wheel-events-in-jquery
+
+var navShowOnScroll
+const checkNavIsMobile = function() {
+  if($(window).width() < 969) {
+    navShowOnScroll = false
+  } else {
+    navShowOnScroll = true
+  }
+}
+
+$( window ).resize(function() {
+  checkNavIsMobile()
+  navOn = false
+})
+
+$(function() {
+  checkNavIsMobile()
+})
+
 $(window).on('wheel', function(event){
   // deltaY obviously records vertical scroll, deltaX and deltaZ exist too
-  if(event.originalEvent.deltaY < 0){
+  if(event.originalEvent.deltaY < 0 && navShowOnScroll){
     // wheeled up
-    $("#nav-comp").fadeIn("fast")
+    $(".nav-holder").fadeIn("fast")
   } else {
     // wheeled down
-    $("#nav-comp").fadeOut("fast")
+    $(".nav-holder").fadeOut("fast")
   }
 })
